8b424ee5ea55e60b1c9e0a1dbcea4972facb1f63
amitknows <amitknowsyou@gmail.com>
2024-05-26 21:13:48 +0530

    final update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonialCard from "@/components/TestimonialCard";
+import Webinars from "@/components/Webinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
-   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+   <main className="min-h-screen bg-black/[0.96] antialiased dark:bg-grid-white/[0.2]">
   
    <HeroSection/>
    <FeaturedCourses/>
    <WhyChooseUs/>
    <MusicSchoolTestimonialCard/>
+   <Webinars/>
+   <Instructors/>
+   {/* <Footer/> */}
    </main>
   );
 }
